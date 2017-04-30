2b21db916350492a1c22c338c43b4b87b1f8fb26
Michael Beilman <beilman.mich@gmail.com>
2017-04-30 16:29:54 -0500

    Initial deploy

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # DonorsChoose_Visualization
-* Source Code for my blog post: [Interactive Data Visualization with D3.js, DC.js, Python, and MongoDB](http://adilmoujahid.com/posts/2015/01/interactive-data-visualization-d3-dc-python-mongodb/)
+# Completed as initial data vizualization project for The Data Incubator week 5.
 
-#Visit my Blog : http://adilmoujahid.com
+# Visit the interactive dashboard here: https://donorschoosedashboard.herokuapp.com/
 
 ## Getting started
 
-The dependencies for the project can be installed using
+The dependencies for the project can be installed (in your virtual environment, venv) using
 
     $ pip install -r requirements.txt
 
@@ -15,8 +15,9 @@ You can use ``Vagrant`` to start a machine with a MongoDB instance running
 
 To initialize the database you need to download the data
 
-    $ wget https://s3.amazonaws.com/open_data/csv/opendata_projects.zip && unzip opendata_projects.zip
+    $ wget http://s3.amazonaws.com/open_data/opendata_projects000.gz
 
-and import it
+
+and import it after gunzip
 
     $ mongoimport -d donorschoose -c projects --type csv --file /vagrant/opendata_projects.csv -headerline
--- school_donations.py
+++ school_donations.py
@@ -1,10 +1,5 @@
-# from flask import Flask
-# from flask import render_template
-# from pymongo import MongoClient
-# import pymongo
-# import json
-# from bson import json_util
-# from bson.json_util import dumps
+#Python 2.7.12
+#Check Heroku Mlab for deployment version - pymongo 3.2.1 specified in requirements.txt
 
 from flask import Flask
 from flask import render_template
@@ -20,16 +15,14 @@ from bson.json_util import dumps
 
 app = Flask(__name__)
 
-# uri = 'mongodb://ltest:test@ds115671.mlab.com:15671/herokup_rqr408rd'
-# client = MongoClient(uri)
-# db = client.get_default_database()
-
+#Keep for local testing#
 #MONGODB_HOST & MONDB_PORT Don't matter for heroku deploy - keep for local testing.
 #MONGODB_HOST = 'localhost'
 #MONGODB_PORT = 27017
-#user:pass@PORT ...
-MONGO_URI = 'mongodb://admin:tdi_test2020@ds115411.mlab.com:15411/mb_donorschoose' #'mongodb://admin:tdi_test2020@ds115411.mlab.com:15411/mb_donorschoose'
-DBS_NAME = 'mb_donorschoose'
+
+#REPLACE WITH RESPECTIVE CREDENTIALS
+MONGO_URI = 'mongodb://<user>:<pass>@ds11XXXX.mlab.com:1XXXX/donorschoose'
+DBS_NAME = 'donorschoose'
 COLLECTION_NAME = 'projects'
 FIELDS = {'school_state': True, 'resource_type': True, 'poverty_level': True, 'date_posted': True, 'total_donations': True, '_id': False}
 
@@ -38,12 +31,12 @@ FIELDS = {'school_state': True, 'resource_type': True, 'poverty_level': True, 'd
 def index():
     return render_template("index.html")
 
-
-@app.route("/mb_donorschoose/projects") #/donorschoose/projects
+@app.route("/donorschoose/projects")
 def donorschoose_projects():
     connection = MongoClient(MONGO_URI)
     collection = connection[DBS_NAME][COLLECTION_NAME]
-    #projects = collection.find(projection=FIELDS, limit=25)
+    #You may be required to test row limits upon deploy, check Heroku logs for H12 error - timeout when calling jqueries. 
+    #You can also combat this with gunicorn specifications found in Procfile -- "web: gunicorn school_donations:app --timeout 30 --keep-alive 5 --log-level debug"
     projects = collection.find( {} ,FIELDS).sort("_id", 1)#.limit(300000)
     #projects = collection.find(projection=FIELDS)
     json_projects = []
